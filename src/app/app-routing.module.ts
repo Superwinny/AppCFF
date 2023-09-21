e236331470e55027cd2daee303e9b4b741ee0c42
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-21 12:56:26 +0200

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [];
 
+
+/**
+ * Faire les deux routes
+ */
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
