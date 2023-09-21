268fa5d6e02ee6d02c61f8ea2ef8c01b4c44abb3
Dos Santos Davide <dos.davide91@gmail.com>
2023-09-21 12:37:18 +0200

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
-import { SerchComponent } from './serch/serch.component';
 import { ResultComponent } from './result/result.component';
+import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    SerchComponent,
-    ResultComponent
+    ResultComponent,
+    SearchComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/search/search.component.spec.ts
+++ src/app/search/search.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SerchComponent } from './serch.component';
+import { SearchComponent } from './search.component';
 
-describe('SerchComponent', () => {
-  let component: SerchComponent;
-  let fixture: ComponentFixture<SerchComponent>;
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [SerchComponent]
+      declarations: [SearchComponent]
     });
-    fixture = TestBed.createComponent(SerchComponent);
+    fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
